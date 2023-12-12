4e669270e4e3a32bac85ff6c09459d0f79b98c47
MaxOvs19 <gshk3669@gmail.com>
2023-12-12 20:05:36 +0300

    DataBase is working

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+import express from "express";
+import cors from "cors";
+import { sequelize } from "./db.js";
+import "dotenv/config";
 
+sequelize;
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log("start server in port:", PORT);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
